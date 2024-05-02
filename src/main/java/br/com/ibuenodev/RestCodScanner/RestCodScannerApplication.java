3ae3defa006fc137f7639806ee8d94e839cd29b8
Isaac Jose Bueno <ibueno.dev@gmail.com>
2024-05-02 14:40:43 -0300

    Criado o projeto

---
+++ src/main/java/br/com/ibuenodev/RestCodScanner/RestCodScannerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibuenodev.RestCodScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCodScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCodScannerApplication.class, args);
+	}
+
+}
